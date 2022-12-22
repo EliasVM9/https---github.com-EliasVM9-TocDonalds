e0381b3e08addf7c9bbd442f6825307cb2cfd0a1
EliasVM9 <eliasville90@gmail.com>
2022-12-21 21:43:05 -0300

    yes

--- TocApp/views.py
+++ TocApp/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
+from .models import Producto, Combo
 from django.http import HttpResponse
-
-
+from TocDonalds import *
+def home(request):
+    Combo = Producto.objects.all()
+    return render(request, "inicio.html", {"Productos": Combo})
+    #return HttpResponse("Hola Mundo")
 
 
 
